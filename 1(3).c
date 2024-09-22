cd76d72187d700a7bf2ac59e34b26522e07eb746
ArtemGGA <artemgaspary@yandex.ru>
2024-09-22 23:10:37 +0300

    пока думаем

--- 1(3).c
+++ 1(3).c
@@ -1,29 +1,86 @@
 #include <stdio.h> 
-/*
-* @brief расчитывает работу по формуле
-* @param time константа time
-* @param I переменная I - сила тока
-* @param U переменная U - напряжение
-* return возвращает рассчитанное значение
-*/
-double work(const int time, double I, double U);
-/*
- * @brief точка входа в программу
- * @return 0 в случае успеха
- */
-int main(void) 
-{ 
-    const int time = 1200.0;
-    double I; 
-    double U;
-    scanf("%lf", &I);
-    scanf("%lf", &U);
-    printf("%lf", work(time, I, U));
+#include <math.h>
 
-    return 0;
+/*
+* @brief proverka проверка на отрицательные числа
+* @param x переменная x
+* @param y переменная y
+* return 1 в случае успеха(число > 0)
+* return 0 в случае, если число отрицательное
+*/
+int proverka(double x, double y);
+
+/*
+ * @brief perimetr рассчитывает периметр
+ * @param x переменная x
+ * @param y переменная y
+ * @return рассчитанное значение 
+ */
+double perimetr(double x, double y);
+
+/*
+ * @brief sqared рассчитывает площадь
+ * @param x переменная x
+ * @param y переменная y
+ * return рассчитанное значение
+ */
+double sqared(double x, double y); 
+
+/*
+ * @brief diagonal рассчитывает диагональ
+ * @param x переменная x
+ * @param y переменная y
+ * return рассчитанное значение
+ */
+double diagonal(double x, double y);
+
+/*
+ * @bried точка входа в программу
+ * return 0 в случае успеха 
+ */
+int main(void)
+{ 
+    double x;
+    double y;
+    while(1)
+    {
+        printf("введите значение x и y:");
+        scanf("%lf %lf", &x, &y);
+        if (proverka(x, y) == 1)
+            break;
+
+    }
+    printf("perimetr- %.3lf\nsqared- %.3lf\ndiagonal- %.3lf", perimetr(x, y), sqared(x, y), diagonal(x, y));
+
+    return 0; 
 }
 
-double work(const int time, double I, double U)
+int proverka(double x, double y)
 { 
-    return U * I * time;
+    if (x > 0 && y > 0) 
+    {
+        printf("проверка прошла успешно");
+        return 1;
+    }
+
+    if (x < 0 && y < 0)
+    { 
+        printf("введите числа >= 0\n");
+        return 0;
+    }
+}
+
+double perimetr(double x, double y)
+{ 
+    return 2 * (x + y);
+}
+
+double sqared(double x, double y)
+{ 
+    return x * y;
+}
+
+double diagonal(double x, double y)
+{ 
+    return sqrt(pow(x, 2) + pow(y, 2));
 }
