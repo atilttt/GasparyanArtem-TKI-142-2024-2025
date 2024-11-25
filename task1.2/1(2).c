e05ae1985e75daf4f875e01057b3e2934371e1a2
shaazmikmlad <artemgaspary@yandex.ru>
2024-11-25 20:08:43 +0300

    opa

---
+++ task1.2/1(2).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * @brief перевод значения веса в граммах в унции
+ * @param inch значение 1 унция
+ * @param x вес в граммах
+ * @return возвращает рассчитаное значение
+ */
+double result(double x);
+
+/**
+ * @brief вход в программу
+ * @return 0 в случае успеха
+ */
+int main(void) 
+{ 
+    double x;
+    printf("Введите вес в граммах:");
+    scanf("%lf", &x);
+    printf("%lf", result(x));
+
+    return 0;
+}
+
+double result(double x)
+{
+    const double unz = 28.3; 
+    return x / unz;
+}
