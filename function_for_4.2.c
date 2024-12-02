ac7d8be829310c75e85fab6c25ee260525a626d8
shaazmikmlad <artemgaspary@yandex.ru>
2024-12-02 16:11:31 +0300

    save

---
+++ function_for_4.2.c
@@ -0,0 +1,123 @@
+#include "function_for_4.2.h"
+
+int input(void) {
+    int value = 0;
+    int result = scanf("%d", &value);
+    if (result != 1) {
+        perror("Ошибка ввода");
+        exit(EXIT_FAILURE);
+    }
+    return value;
+}
+
+int pozitiv_input(void) {
+    int value = input();
+    if (value <= 0) {
+        perror("Ошибка ввода: число должно быть > 0");
+        exit(EXIT_FAILURE);
+    }
+    return value;
+}
+
+int* create_array(const size_t n) {
+    int *array = (int *)malloc(n * sizeof(int));
+    check_array(array);
+    return array;
+}
+
+int* copy_array(const int *array, const size_t n) {
+    check_array(array);
+    int *new_array = create_array(n);
+    for (size_t i = 0; i < n; ++i) {
+        new_array[i] = array[i];
+    }
+    return new_array;
+}
+
+void check_array(const int *array) {
+    if (array == NULL) {
+        perror("Ошибка выделения памяти");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void random_filling(int *array, const size_t n) {
+    check_array(array);
+    printf("Введите минимальное и максимальное значения:\n");
+    int min = input();
+    int max = input();
+    if (min > max) {
+        printf("Ошибка: минимальное значение больше максимального.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < n; ++i) {
+        array[i] = rand() % (max - min + 1) + min;
+    }
+}
+
+void user_input_filling(int *array, const size_t n) {
+    check_array(array);
+    for (size_t i = 0; i < n; ++i) {
+        printf("Введите элемент %zu:\n", i + 1);
+        array[i] = input();
+    }
+}
+
+void print_array(const int *array, const size_t n) {
+    printf("Ваш массив:\n{");
+    for (size_t i = 0; i < n; ++i) {
+        printf("%d ", array[i]);
+    }
+    printf("}\n");
+}
+
+int* replacement(int *array, const size_t n) {
+
+    int *array_2 = copy_array(array, n);
+    for (size_t i = 0; i < n; ++i) {
+        if (array_2[i] < 0) {
+            array_2[i] = 0;
+            break;
+        }
+    }
+
+    return array_2;
+}
+
+size_t count_elementary(const int *array, size_t n) {
+    size_t counter = 0;
+    for (size_t i = 0; i < n; i++) {
+        if (array[i] % (i + 1) == 0) {
+            ++counter;
+        }
+    }
+    return counter;
+}
+
+int* create_resized_array(const int *array, const size_t n, const size_t counter, const int k) {
+    size_t new_n = n + counter;
+    int *new_array = create_array(new_n);
+
+    size_t index = 0;
+    for (size_t i = 0; i < n; i++) {
+        new_array[index++] = array[i];
+        if (array[i] % (i + 1) == 0) {
+            new_array[index++] = k;
+        }
+    }
+    return new_array;
+}
+
+int* array_from_array(const int *array, const size_t n) {
+    int *arr = create_array(n);
+
+    for (size_t i = 0; i < n; ++i) {
+        if ((i + 1) % 2 == 0) {
+            arr[i] = (i - 1) * array[i];
+        } else {
+            arr[i] = array[i] * i * 2;
+        }
+    }
+    return arr;
+}
---
+++ function_for_4.2.h
@@ -0,0 +1,117 @@
+#ifndef FUNCTION_FOR_4_2_H
+
+#define FUNCTION_FOR_4_2_H
+
+#include <stdio.h> 
+#include <errno.h>
+#include <stdlib.h> 
+#include <math.h>
+#include <time.h>
+#include <locale.h>
+#include <windows.h>  
+
+
+/**
+ * @brief функция проверяет пользовательский ввод
+ * @return возвращает ошибку в случае, если пользователь ввел некоректное значение
+ */
+int input(void);
+
+/**
+ * @brief функция проверяет ввел ли пользователь положительное число
+ * @return возвращает ошибку в случае, если число отрицательное
+ */
+int pozitiv_input(void);
+
+/**
+ * @brief функция, создающая массив
+ * @param n переменная, отвечающая за размер массива
+ * @return созданный массив
+ */
+int* create_array(const size_t n);
+
+/**
+ * @brief функция, копирающая массив 
+ * @param *array указатель на массив (эквивалентно array[])
+ * @param n переменная, отвечающая за размер массива
+ * @return скопированный массив
+ */
+int* copy_array(const int *array, const size_t n);
+
+/**
+ * @brief функция проверяющая значение адресса
+ * @param *array - указатель на массив (эквивалентно array[])
+ */
+void check_array(const int *array);
+
+/**
+ * @param INPUT заполнение пользователем
+ * @param RANDOM заполнение рандомными числами
+ */
+enum choice { 
+    INPUT_TYPE = 0, 
+    RANDOM = 1
+};
+
+/**
+ * @brief функция, выполняющая заполнение массива рандомными числами
+ * @param array указатель на массив (эквивалентно array[])
+ * @param n переменная, отвечающая за размер массива
+ */
+void random_filling(int *array, const size_t n);
+
+/**
+ * @brief функция, выполняюзая заполнение массива с помощью пользовательского ввода
+ * @param array указатель на массив (эквивалентно array[])
+ * @param n переменная, отвечающая за размер массива
+ */
+void user_input_filling(int *array, const size_t n);
+
+/**
+ * @brief функция, которая выводит массив
+ * @param array указатель на массив (эквивалентно array[])
+ * @param n переменная, отвечающая за размер массива
+ */
+void print_array(const int *array, const size_t n);
+
+/**
+ * @brief функция создает новый массив на основе прошлого 
+ * @param arrey указатель на массив (эквивалентно array[])
+ * @param n переменная, отвечающая за размер массива
+ */
+int* copy_array(const int *array, const size_t n);
+
+/**
+ * @brief функция находит первый отрицательный элемент в массиве и меняет его на 0
+ * @param array указатель на массив (эквивалентно array[])
+ * @param n переменная, отвечающая за размер массива
+ */
+int* replacement(int *array, const size_t n);
+
+/** 
+ * @brief считает элементы массива, которые кратны своему номеру
+ * @param *array указатель на массив (эквивалентно array[])
+ * @param n переменная, отвечающая за размер массива
+ * @return возвращает кол-во таких элементов
+ */
+size_t count_elementary(const int *array, size_t n);
+
+/**
+ * @brief функция вставляет элемент k, после каждого элемента массива, кратного своему номеру
+ * @param *array указатель на массив (эквивалентно array[])
+ * @param n переменная, отвечающая за размер массива
+ * @param counter переменная-счетчик (хранит в себе кол-во элементов, кратных своему номеру)
+ * @param k переменная, хранящая в себе произвольное значение, введенное пользователем, которое вставляется после каждого элемента, кратного своему номеру
+ * @return массив с вставленным в него числом K
+ */
+int* create_resized_array(const int *array, const size_t n, const size_t counter, const int k);
+
+/**
+ * @brief функция релизует массив А на основе массива array
+ * @param указатель на массив (эквивалентно array[]) 
+ * @param n переменная, отвечающая за размер массива
+ */
+int* array_from_array(const int *array, const size_t n);
+
+
+#endif // FUNCTION_FOR_4_2_H
