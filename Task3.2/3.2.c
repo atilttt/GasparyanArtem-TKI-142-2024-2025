9f3dffe41f6249e0f47aedd1a6b2342a5d33c263
shaazmikmlad <artemgaspary@yandex.ru>
2024-12-02 15:53:55 +0300

    save

--- Task3.2/3.2.c
+++ Task3.2/3.2.c
@@ -6,66 +6,68 @@
 
 /**
  * @brief получает на вход число из потока ввода
- * @return возвращает проверенное число, в противном случае завершает программу с ошибкой
+ * @return возвращает проверенное число, в противном случае ошибку
  */
 double input(void);
 
 /**
- * @brief получает на вход целое число из потока ввода
- * @return возвращает проверенное целое число, в противном случае завершает программу с ошибкой
+ * @brief получает на вход число из потока ввода
+ * @return возвращает провернное число, в противном случае ошибку 
  */
 int integer_input(void);
 
 /**
- * @brief проверяет, что введенное число является положительным целым числом
- * @return возвращает положительное целое число, иначе завершает выполнение программы
+ * @brief проверяет заданное число на положительность
+ * @return возвращает проверенное число, в ином случае завершает программу и пишет ошибку
  */
 int positive_int(void);
 
 /**
- * @brief проверяет, что введенное число является положительным (вещественное число)
- * @return возвращает положительное число, иначе завершает выполнение программы
+ * @brief проверяет заданное число на положительность 
+ * @return возвращает проверенное число, в ином случае завершает программу и пишет ошибку
  */
-double positive_double(void);
+double pozitive_double(void);
 
 /**
- * @brief вычисляет следующий член последовательности по формуле f(k) = -k^4 / k! (какая-то ошибка была)
- * @param k Текущий индекс последовательности
+ * @brief вычисляет следующий член последовательности при помощи рекуррентного выражения
+ * @param k текущий индекс последовательности
  * @return значение следующего члена последовательности
  */
-double next_term(const int k);
+double next_term(const int k); // Убрали `previous_term`, добавили `const` //
 
 /**
- * @brief рассчитывает сумму первых `n` членов последовательности
- * @param n Количество первых членов последовательности
- * @return сумма первых `n` членов
+ * @brief рассчитывает значение суммы первых n-членов последовательности
+ * @param n кол-во элементов последовательности
+ * @return рассчитанное значение суммы
  */
 double get_sum_first_n(const int n);
 
 /**
- * @brief рассчитывает сумму всех членов последовательности, которые больше или равны заданного `e`
- * @param e Значение точности (ограничивающий член последовательности)
- * @return сумма членов, которые больше или равны `e`
+ * @brief рассчитывает значение суммы всех членов последовательности, не меньше заданного числа е
+ * @param n кол-во элементов последовательности
+ * @param e значение заданного числа е
+ * @return рассчитанное значение суммы
  */
-double get_sum_dependent_e(const double e);
+double get_sum_dependet_e(const double e);
 
 /**
- * @brief Точка входа в программу
- * @return 0 в случае успешного выполнения
+ * @brief точка входа в программу 
+ * @return 0 в случае успеха
  */
-int main(void) {
-    printf("Please enter the number of sequence elements (n):\n");
+int main(void)
+{
+    printf("Please enter the number of sequence elements:\n");
     const int n = positive_int();
-    printf("Please enter value e (precision):\n");
-    const double e = positive_double();
-
-    printf("The sum of the first n terms of the sequence: %.3lf\n", get_sum_first_n(n));
-    printf("The sum of all the members of the sequence not less than e: %.3lf\n", get_sum_dependent_e(e));
+    printf("Please enter value e:\n");
+    const double e = pozitive_double();
+    printf("The sum of the first n terms of the sequence = %.3lf\n", get_sum_first_n(n));
+    printf("The sum of all the members of the sequence, not less than a given number e = %.3lf\n", get_sum_dependet_e(e));
 
     return 0; 
 }
 
-int integer_input(void) { 
+int integer_input(void)
+{ 
     int value = 0; 
     int result = scanf("%d", &value);
     if (result != 1) {
@@ -77,77 +79,73 @@ int integer_input(void) {
     return value;
 }
 
-int positive_int(void) {
-    int value = integer_input();
-    if (value <= 0) { 
-        errno = EINVAL;
-        perror("Value must be a positive integer");
+int positive_int(void)
+{
+    int value =  integer_input();
+    if (value < 0)
+    { 
+        errno = EIO;
+        perror("Impossible value for n");
         exit(EXIT_FAILURE);
     }
 
     return value;
 }
 
-double input(void) { 
+double input(void)
+{ 
     double parameter = 0.0;
     int outcome = scanf("%lf", &parameter);
     if (outcome != 1) { 
         errno = EIO; 
-        perror("Input error");
+        perror("Input error\n");
         exit(EXIT_FAILURE);
     }
 
     return parameter;
 }
 
-double positive_double(void) { 
+double pozitiv_double(void)
+{ 
     double parameter = input();
-    if (parameter <= DBL_EPSILON) { 
-        errno = EINVAL;
-        perror("Value must be a positive number");
+    if (parameter <= DBL_EPSILON)
+    { 
+        errno = EIO;
+        perror("Input error\n");
         exit(EXIT_FAILURE);
     }
 
     return parameter;
 }
 
-/**
- * @brief Вычисление элемента последовательности по формуле: (-1)^k * (k^4 / k!)
- */
 double next_term(const int k) {
-    static double factorial = 1.0; // Рекуррентное вычисление факториала
-    factorial *= (k == 0) ? 1.0 : k; // Если k == 0, то факториал остается 1
-    return pow(-1, k) * (pow(k, 4) / factorial); // Элемент ряда: (-1)^k * (k^4 / k!)
+    return -1 * (pow(k, 4) / k); 
 }
 
-/**
- * @brief Расчет суммы первых n членов последовательности
- */
-double get_sum_first_n(const int n) {
-    double sum = 0.0; 
+double get_sum_first_n(const int n)
+{
+    double term = 1.0; 
+    double sum = term; // Начальный член a_1 = 1^4 / 1!
 
-    for (int k = 0; k < n; ++k) {
-        sum += next_term(k); 
+    for (int k = 1; k < n; ++k) {
+        term *= next_term(k); 
+        sum += term;
     } 
 
     return sum;
 }
 
-/**
- * @brief Расчет суммы членов ряда, модуль которых >= e
- */
-double get_sum_dependent_e(const double e) {
-    double sum = 0.0;
-    double term = 0.0;  
-    int k = 0;
-
-    do {
-        term = next_term(k); 
-        if (fabs(term) >= e) {
-            sum += term;
-        }
-        k++;
-        } while (fabs(term) >= e);
-
+double get_sum_dependet_e(const double e) 
+{
+    double sum = 1.0;
+    double term = 1.0;  // Начальный член a_1 = 1^4 / 1!
+    int k = 1;
+    while (fabs(term) >= e + DBL_EPSILON)
+    {
+        sum += term;
+        term *= next_term(k); 
+        k++;   
+    }
+    
     return sum;
 }
